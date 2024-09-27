0f8c6dfa166413749f6e7a9b1943bc78373a1b95
Angelbyte <roberto.angel96@live.com>
2024-09-27 15:19:27 -0600

    Refactorizar el componente FollowMouse en App.jsx y actualizar estilos en index.css

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,74 +1,68 @@
 import { useEffect, useState } from 'react'
 
 const FollowMouse = () => {
-  const [enabled, setEnabled] = useState(false)
-  const [position, setPosition] = useState({ x: 0, y: 0 })
+	const [enabled, setEnabled] = useState(false)
+	const [position, setPosition] = useState({ x: 0, y: 0 })
 
-  // pointer move
-  useEffect(() => {
-    console.log('effect ', { enabled })
+	// pointer move
+	useEffect(() => {
+		console.log('efecto', { enabled })
 
-    const handleMove = (event) => {
-      const { clientX, clientY } = event
-      setPosition({ x: clientX, y: clientY })
-    }
+		const handleMove = e => {
+			const { clientX, clientY } = e
+			console.log('handleMove', { clientX, clientY })
+			setPosition({ x: clientX, y: clientY })
+		}
 
-    if (enabled) {
-      window.addEventListener('pointermove', handleMove)
-    }
+		if (enabled) {
+			window.addEventListener('pointermove', handleMove)
+		}
 
-    // cleanup:
-    // -> cuando el componente se desmonta
-    // -> cuando cambian las dependencias, antes de ejecutar
-    //    el efecto de nuevo
-    return () => { // cleanup method
-      console.log('cleanup')
-      window.removeEventListener('pointermove', handleMove)
-    }
-  }, [enabled])
+		return () => {
+			console.log('cleanup')
+			window.removeEventListener('pointermove', handleMove)
+		}
+	}, [enabled])
 
-  // [] -> solo se ejecuta una vez cuando se monta el componente
-  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
-  // undefined -> se ejecuta cada vez que se renderiza el componente
+	// [] -> solo se ejecuta una vez cuando se monta el componente
+	// [enabled] -> se ejecuta cuando cambia enabled y se monta el componente
+	// undefined -> se ejecuta cada vez que se renderiza el componente
 
-  // change body className
-  useEffect(() => {
-    document.body.classList.toggle('no-cursor', enabled)
+	// change body className
+	useEffect(() => {
+		document.body.classList.toggle('no-cursor', enabled)
 
-    return () => {
-      document.body.classList.remove('no-cursor')
-    }
-  }, [enabled])
+		return () => {
+			document.body.classList.remove('no-cursor')
+		}
+	}, [enabled])
 
-  return (
-    <>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        border: '1px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(${position.x}px, ${position.y}px)`
-      }}
-      />
-      <button onClick={() => setEnabled(!enabled)}>
-        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
-      </button>
-    </>
-  )
+	return (
+		<>
+			<div
+				style={{
+					position: 'absolute',
+					backgroundColor: 'rgba(0, 0, 0, 0.5)',
+					border: '1px solid #fff',
+					borderRadius: '50%',
+					opacity: 0.8,
+					pointerEvents: 'none',
+					left: -10,
+					top: -10,
+					width: 20,
+					height: 20,
+					transform: `translate(${position.x}px, ${position.y}px)`
+				}}
+			/>
+			<button onClick={() => setEnabled(!enabled)}>
+				{enabled ? 'Desactivar' : 'Activar'} seguir puntero
+			</button>
+		</>
+	)
 }
 
-function App () {
-  return (
-    <main>
-      <FollowMouse />
-    </main>
-  )
+const App = () => {
+	return <main>{<FollowMouse />}</main>
 }
 
 export default App
