71e08c52cd9c120f1dac2c2e63e5e6205365dd05
Angelbyte96 <roberto.angel96@live.com>
2024-12-30 01:15:22 -0600

    Agregar manejo de resultados sin películas, actualizar configuración de ESLint y refactorizar componentes

--- projects/05-react-buscador-peliculas/src/components/Movies.jsx
+++ projects/05-react-buscador-peliculas/src/components/Movies.jsx
@@ -1,31 +1,26 @@
-function ListOfMovies ({ movies }) {
-  return (
-    <ul className='movies'>
-      {
-        movies.map(movie => (
-          <li className='movie' key={movie.id}>
-            <h3>{movie.title}</h3>
-            <p>{movie.year}</p>
-            <img src={movie.image} alt={movie.title} />
-          </li>
-        ))
-      }
-    </ul>
-  )
+/* eslint-disable react/prop-types */
+const ListOfMovies = ({ movies }) => {
+	return (
+		<ul>
+			{movies.map(movie => (
+				<li key={movie.id}>
+					<h3>{movie.title}</h3>
+					<p>{movie.year}</p>
+					<img src={movie.image} alt={movie.title} />
+				</li>
+			))}
+		</ul>
+	)
 }
 
-function NoMoviesResults () {
-  return (
-    <p>No se encontraron películas para esta búsqueda</p>
-  )
+const NoMoviesResults = () => {
+	return <p>No se encontraron resultados</p>
 }
 
-export function Movies ({ movies }) {
-  const hasMovies = movies?.length > 0
+const Movies = ({ movies }) => {
+	const hasMovies = movies?.length > 0
 
-  return (
-    hasMovies
-      ? <ListOfMovies movies={movies} />
-      : <NoMoviesResults />
-  )
+	return hasMovies ? <ListOfMovies movies={movies} /> : <NoMoviesResults />
 }
+
+export { Movies }
--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,38 +1,17 @@
-import { useRef, useState, useMemo, useCallback } from 'react'
-import { searchMovies } from '../services/movies.js'
+import responseMovies from '../notes/with-results.json'
+import withoutResults from '../notes/no-results.json'
 
-export function useMovies ({ search, sort }) {
-  const [movies, setMovies] = useState([])
-  const [loading, setLoading] = useState(false)
-  // el error no se usa pero puedes implementarlo
-  // si quieres:
-  const [, setError] = useState(null)
-  const previousSearch = useRef(search)
+const useMovies = () => {
+	const movies = responseMovies.Search
 
-  const getMovies = useCallback(async ({ search }) => {
-    if (search === previousSearch.current) return
-    // search es ''
+	const mappedMovies = movies?.map(movie => ({
+		id: movie.imdbID,
+		title: movie.Title,
+		year: movie.Year,
+		image: movie.Poster
+	}))
 
-    try {
-      setLoading(true)
-      setError(null)
-      previousSearch.current = search
-      const newMovies = await searchMovies({ search })
-      setMovies(newMovies)
-    } catch (e) {
-      setError(e.message)
-    } finally {
-      // tanto en el try como en el catch
-      setLoading(false)
-    }
-  }, [])
-
-  const sortedMovies = useMemo(() => {
-        if (!movies) return;
-    return sort
-      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
-      : movies
-  }, [sort, movies])
-
-  return { movies: sortedMovies, getMovies, loading }
+	return { movies: mappedMovies }
 }
+
+export { useMovies }
